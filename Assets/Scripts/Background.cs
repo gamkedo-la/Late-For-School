804fc994e31a826666dd645af6175b318ab82cce
Christer Kaitila <breakdance@mcfunkypants.com>
2020-12-31 12:21:21 -0800

    background parallax effect (wip)

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
+    public float speedScale = 1f;
     public List<SpriteRenderer> backgroundSprites;
     public List<float> backgroundSpeeds;
 
@@ -18,13 +19,17 @@ public class Background : MonoBehaviour
     {
         for (int i = 0; i < backgroundSprites.Count; i++)
         {      
-            float currentscroll = Time.time * backgroundSpeeds[i];
+            float currentscroll = Mathf.Repeat (Time.time * backgroundSpeeds[i],1); // 0..1 loop
             
             // appears to work
             // Debug.Log(Time.time + " bg layer " + i + " currentscroll: "+currentscroll);
             
-            // no error.. but this seems to do nothing..
+            // no error.. but this seems to do nothing.. only works on a 3d material?!? not sprite
             backgroundSprites[i].material.mainTextureOffset = new Vector2(currentscroll, 0);
+
+            // HACK FIXME - move the actual sprites lol =(
+            float newX = Mathf.Repeat (Time.time * -1 * backgroundSpeeds[i] * speedScale,8);
+            backgroundSprites[i].transform.position = new Vector2(newX,backgroundSprites[i].transform.position.y);
         }
     }
 }
