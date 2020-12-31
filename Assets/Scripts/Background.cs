a4a1997ad74b82b8699ce2b7ebb31f1f51a2c8cf
Christer Kaitila <breakdance@mcfunkypants.com>
2020-12-31 12:00:25 -0800

    background parallax sprite layers

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public List<SpriteRenderer> backgroundSprites;
+    public List<float> backgroundSpeeds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < backgroundSprites.Count; i++)
+        {      
+            float currentscroll = Time.time * backgroundSpeeds[i];
+            
+            // appears to work
+            // Debug.Log(Time.time + " bg layer " + i + " currentscroll: "+currentscroll);
+            
+            // no error.. but this seems to do nothing..
+            backgroundSprites[i].material.mainTextureOffset = new Vector2(currentscroll, 0);
+        }
+    }
+}
