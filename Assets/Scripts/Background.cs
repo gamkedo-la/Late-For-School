899cb38194f7fcb48b2a6242fe9ae32f200950ab
Christer Kaitila <breakdance@mcfunkypants.com>
2021-01-04 16:24:06 -0800

    fabulous new background parallax effect using quads instead of sprites for perfect UV wrapping

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     public float speedScale = 1f;
-    public List<SpriteRenderer> backgroundSprites;
+    public List<MeshRenderer> backgroundQuads;
     public List<float> backgroundSpeeds;
 
     // Start is called before the first frame update
@@ -17,19 +17,13 @@ public class Background : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < backgroundSprites.Count; i++)
+        for (int i = 0; i < backgroundQuads.Count; i++)
         {      
-            float currentscroll = Mathf.Repeat (Time.time * backgroundSpeeds[i],1); // 0..1 loop
-            
-            // appears to work
-            // Debug.Log(Time.time + " bg layer " + i + " currentscroll: "+currentscroll);
-            
-            // no error.. but this seems to do nothing.. only works on a 3d material?!? not sprite
-            backgroundSprites[i].material.mainTextureOffset = new Vector2(currentscroll, 0);
-
-            // HACK FIXME - move the actual sprites lol =(
-            float newX = Mathf.Repeat (Time.time * -1 * backgroundSpeeds[i] * speedScale,8);
-            backgroundSprites[i].transform.position = new Vector2(newX,backgroundSprites[i].transform.position.y);
+            // sprites can't update UV but 3d meshes can
+            if (backgroundQuads[i]) {
+                float currentscroll = Time.time * backgroundSpeeds[i] * speedScale;
+                backgroundQuads[i].material.mainTextureOffset = new Vector2(currentscroll, 0);
+            }
         }
     }
 }
