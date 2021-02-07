796ef17906b3ffc3ecb25b4621b9f37275987e3d
Praneil Kamat <pkamat@mac.com>
2021-02-07 11:22:54 +0000

    allow player to climb sides of screen while in menu

---
+++ Assets/Scripts/BoundsController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BoundsController : MonoBehaviour
+{
+    public GameObject deathBounds;
+    public GameObject rightBoundsNoFriction;
+    public GameObject leftBoundsFriction;
+    public GameObject rightBoundsFriction;
+    public GameObject topBounds;
+
+    void Update()
+    {
+        if (GameManager.GetInstance().GetState() == GameManager.GameState.Play)
+        {
+            deathBounds.SetActive(true);
+            rightBoundsNoFriction.SetActive(true);
+            leftBoundsFriction.SetActive(false);
+            rightBoundsFriction.SetActive(false);
+            topBounds.SetActive(false);
+        }
+        else
+        {
+            deathBounds.SetActive(false);
+            rightBoundsNoFriction.SetActive(false);
+            leftBoundsFriction.SetActive(true);
+            rightBoundsFriction.SetActive(true);
+            topBounds.SetActive(true); // Prevents player from infinitely climbing up on either wall
+        }
+    }
+}
