bf20783663d26cc2fc3a0aeaf73033e359878414
Praneil Kamat <pkamat@mac.com>
2021-02-14 22:16:53 +0000

    make player get hurt by puddle if still in it and invincibility timer runs out

--- Assets/Scripts/StationaryDangerousObstacle.cs
+++ Assets/Scripts/StationaryDangerousObstacle.cs
@@ -7,7 +7,7 @@ public class StationaryDangerousObstacle : MonoBehaviour
     [FMODUnity.EventRef]
     public string playerHitSound;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
