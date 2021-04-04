9c8c38b64a4abcc0c906232807713128d7b9d604
Praneil Kamat <pkamat@mac.com>
2021-04-05 00:18:27 +0100

    prevent any menu blocks from being hit half a second after any others have been hit (solves issue where you hit one that just appeared after hitting another one)

--- Assets/Scripts/MenuBlock.cs
+++ Assets/Scripts/MenuBlock.cs
@@ -9,6 +9,10 @@ public class MenuBlock : MonoBehaviour
 
     public UnityEvent onHit;
 
+    private static float hitTimeOutSeconds = 0.5f;
+    private static float hitTimeOutLeft = 0;
+    public static bool hitTimeOutUpdatedThisFrame = false;
+
     private BoxCollider2D boxCollider2D;
     public delegate void PlayerCollisionAction();
 
@@ -17,15 +21,36 @@ public class MenuBlock : MonoBehaviour
         boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
+    public void Update()
+    {
+        hitTimeOutUpdatedThisFrame = false;
+    }
+
+    public void LateUpdate()
+    {
+        // Prevent more than one class from updating the timer each frame
+        if (!hitTimeOutUpdatedThisFrame)
+        {
+            hitTimeOutLeft -= Time.deltaTime;
+            hitTimeOutUpdatedThisFrame = true;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.bounds.max.y <= boxCollider2D.bounds.min.y &&
             collision.collider.bounds.min.x < boxCollider2D.bounds.max.x &&
             collision.collider.bounds.max.x > boxCollider2D.bounds.min.x &&
-            collision.collider.CompareTag("Player"))
+            collision.collider.CompareTag("Player") &&
+            hitTimeOutLeft <= 0)
         {
             onHit.Invoke();
             FMODUnity.RuntimeManager.PlayOneShot(blockHitSound);
         }
+    }
+
+    private void OnEnable()
+    {
+        hitTimeOutLeft = hitTimeOutSeconds;
     }
 }
