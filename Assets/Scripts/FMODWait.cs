571453d336824a12e803b0317c491c587ca41d9a
Praneil Kamat <pkamat@mac.com>
2021-04-25 21:45:25 +0100

    get music working in webgl

---
+++ Assets/Scripts/FMODWait.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FMODWait : MonoBehaviour
+{
+    public string bankToLoad;
+    public string sceneToLoad;
+
+    void Update()
+    {
+        if (FMODUnity.RuntimeManager.HasBankLoaded(bankToLoad))
+        {
+            Debug.Log("Master Bank Loaded");
+            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+        }
+    }
+}
